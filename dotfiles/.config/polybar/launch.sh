a45be4a8a58789131ea394bd1fdaf0c64903331d
isaac09809 <joseluis-jl-098@hotmail.com>
2024-02-27 21:36:45 -0600

    20240227

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -8,13 +8,6 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar background 2>&1 | tee -a /tmp/polybar1.log & disown
-sleep 0.1
-polybar workspaces 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar music 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar info 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar utilities 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar hour 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar date 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
